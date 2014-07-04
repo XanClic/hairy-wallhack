2b5948c104aceb876fd4b1fa1fbd13a704c99921
Hanna Reitz <xanclic@xanclic.moe>
2014-07-04 23:27:25 +0200

    dake, 5.1
    + Get dake in this thing
    + 5.1

---
+++ src/flappy_box/model/paddle.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include "flappy_box/model/paddle.hpp"
+
+
+using namespace model;
+using namespace flappy_box::model;
+
+
+Paddle::Paddle(const std::string &name):
+  GameObject(true, name)
+{}
---
+++ src/flappy_box/model/world.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include "flappy_box/model/world.hpp"
+
+
+using namespace model;
+using namespace flappy_box::model;
+
+
+World::World(const std::string &name):
+  GameObject(false, name)
+{}
