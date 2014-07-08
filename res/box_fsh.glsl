2abe604d61a316fedb7ed77255c94475c932b345
Max Reitz <xanclic@gmail.com>
2014-07-08 20:40:00 +0200

    Simplify box OBJ, fix box drawing
    * Box OBJ simplified
    ! Box drawing uses absolute light position, adds ambient to diffuse
      before multiplying by texture, normal matrix is calculated from
      modelview matrix and not from MVP matrix, diffuse and ambient base are
      set and used

--- res/box_fsh.glsl
+++ res/box_fsh.glsl
@@ -1,19 +1,21 @@
 #version 150 core
 
+in vec3 vf_position;
 in vec2 vf_texcoord;
 in vec3 vf_normal;
 
 out vec4 out_mi, out_hi;
 
-uniform vec3 light_dir, ambient;
+uniform vec3 light_pos, ambient, diffuse_base;
 uniform float enlightenment;
 uniform sampler2D tex;
 
 
 void main(void)
 {
-  float diff_co = dot(normalize(vf_normal), normalize(-light_dir));
-  vec3 col = enlightenment * (ambient + diff_co * texture(tex, vf_texcoord).rgb);
+  vec3 inv_light_dir = light_pos - vf_position;
+  float diff_co = max(0.0, dot(normalize(vf_normal), normalize(inv_light_dir)));
+  vec3 col = enlightenment * (ambient + diff_co * diffuse_base) * texture(tex, vf_texcoord).rgb;
 
   out_mi = vec4(col, 1.0);
   out_hi = vec4(col / 10.0, 1.0);
