f658fc611b7802de97495dc24e2106a77543bb92
Hanna Reitz <xanclic@xanclic.moe>
2014-07-15 21:27:44 +0200

    Remove unused code

--- src/flappy_box/controller/explosion_logic.cpp
+++ src/flappy_box/controller/explosion_logic.cpp
@@ -33,15 +33,6 @@ bool ExplosionLogic::advance(Logic &l, const InputEventHandler::keyboard_event &
     _model->particle_velocities()[i] += (drag + vec3_type(0.f, -9.81f, 0.f)) * timestep;
     _model->particle_positions()[i]  += _model->particle_velocities()[i] * timestep;
 
-    /*
-    for (int j = 0; j < 3; j++) {
-      if (fabsf(_model->particle_positions()[i][j]) >_model->max_position()[j]) {
-        _model->particle_positions()[i][j] = copysignf(_model->max_position()[j], _model->particle_positions()[i][j]);
-        _model->particle_velocities()[i][j] *= -1.f;
-      }
-    }
-    */
-
     bool any_collision = false;
     scalar_type old_speed = _model->particle_velocities()[i].length();
 
